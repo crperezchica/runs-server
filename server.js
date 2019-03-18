564f6f283da8ca4b6d3241ec40da5bd0c007b573
Carmen Perezchica <crperezchica@gmail.com>
2019-03-18 16:56:58 -0700

    fixed path issue

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
+
 const PORT = process.env.PORT || 7891;
 
-app.listen(PORT, () => console.log('listening on ${PORT}'));
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
